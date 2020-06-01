a1cff8d84a57b490427240398335ccd214412c32
TerriFrid <61093887+TerriFrid@users.noreply.github.com>
2020-06-01 09:54:53 -0400

    Adding about, favicon, and database content

--- Controllers/BlogPostsController.cs
+++ Controllers/BlogPostsController.cs
@@ -81,9 +81,7 @@ namespace TFridBlog.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-
-               
+        [ValidateAntiForgeryToken]               
         public ActionResult Create([Bind(Include = "Title,Abstract,Body,MediaUrl,IsPublished")] BlogPost blogPost,HttpPostedFileBase image)
         {
             if (ModelState.IsValid)
